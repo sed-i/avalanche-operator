1dbc3980560dc9c6317090ae551e04a31903243e
sed-i <82407168+sed-i@users.noreply.github.com>
2021-10-01 17:23:22 -0400

    add metrics

--- tools/bench_metrics_provider.py
+++ tools/bench_metrics_provider.py
@@ -12,19 +12,32 @@ import urllib.error
 import urllib.parse
 import urllib.request
 
+# _percent
+# _sec
+# _up
+
 # Create a metric to track time spent and requests made.
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
+REQUEST_TIME = Summary('request_processing_sec', 'Time spent processing request')
 cpu_avg = deque(maxlen=10)
 cpu_percent = Gauge('cpu_percent', 'CPU %')
 vmem_percent = Gauge('vmem_percent', "Virtual memory %")
 smem_percent = Gauge('smem_percent', "Swap memory %")
-scrape_duration = Gauge('scrape_duration', 'Scrape duration')
+disk_percent = Gauge('disk_percent', "Disk %")
+scrape_duration_sec = Gauge('scrape_duration_sec', 'Scrape duration')
 scrape_duration_percent = Gauge('scrape_duration_percent', 'Scrape duration %')
-scrape_interval = Gauge('scrape_interval', 'Scrape interval')
+scrape_interval_sec = Gauge('scrape_interval_sec', 'Scrape interval')
 
-prom_scraped_avalanche_successfully = Gauge(
-    'prom_scraped_avalanche_successfully',
+prom_scrape_avalanche_up = Gauge(
+    'prom_scrape_avalanche_up',
     'Prom scraped avalanche successfully')
+
+juju_up = Gauge(
+    'juju_up',
+    'Juju is responsive')
+
+vmem_gb = Gauge('vmem_gb', "virtual memory [GB]")
+smem_gb = Gauge('smem_gb', "swap memory [GB]")
+disk_gb = Gauge('disk_gb', "disk [GB]")
 
 
 def get_stdout(args: list):
@@ -50,7 +63,12 @@ def get_json_from_url(url: str, timeout: float = 2.0) -> dict:
 
 
 def get_prom_address(unit="prometheus/0"):
-    unit_info = yaml.safe_load(get_stdout(['juju', 'show-unit', unit]))
+    try:
+        unit_info = yaml.safe_load(get_stdout(['juju', 'show-unit', unit]))
+        juju_up.set(1)
+    except:
+        juju_up.set(0)
+        raise
     return unit_info[unit]["address"]
 
 
@@ -58,7 +76,7 @@ def get_scrape_duration():
     targets_url = f"http://{get_prom_address()}:9090/api/v1/targets"
     targets_info = get_json_from_url(targets_url)  # json.loads(get_stdout(["curl", "--no-progress-meter", targets_url]))
     ours = list(filter(lambda target: target["discoveredLabels"]["__address__"] == "192.168.1.101:9001", targets_info["data"]["activeTargets"]))
-    prom_scraped_avalanche_successfully.set(int(ours[0]["health"] == "up"))
+    prom_scrape_avalanche_up.set(int(ours[0]["health"] == "up"))
     return ours[0]["lastScrapeDuration"]
 
 
@@ -77,8 +95,18 @@ def get_scrape_interval() -> int:
 def process_sys_metrics():
     cpu_avg.append(psutil.cpu_percent())
     cpu_percent.set(sum(cpu_avg)/len(cpu_avg))
-    vmem_percent.set(psutil.virtual_memory().percent)
-    smem_percent.set(psutil.swap_memory().percent)
+
+    vm = psutil.virtual_memory()
+    vmem_percent.set(vm.percent)
+    vmem_gb.set(vm.total / 1e9 * (vm.percent / 100))
+
+    sm = psutil.swap_memory()
+    smem_percent.set(sm.percent)
+    smem_gb.set(sm.used / 1e9)
+
+    disk = psutil.disk_usage("/var/lib/prometheus")
+    disk_percent.set(disk.percent)
+    disk_gb.set(disk.used / 1e9)
 
 
 if __name__ == '__main__':
@@ -101,10 +129,10 @@ if __name__ == '__main__':
                 with REQUEST_TIME.time():
                     sd = get_scrape_duration()
                     si = get_scrape_interval()
-                    scrape_duration.set(sd)
-                    scrape_interval.set(si)
+                    scrape_duration_sec.set(sd)
+                    scrape_interval_sec.set(si)
                     scrape_duration_percent.set(sd/si * 100)
             except:
-                prom_scraped_avalanche_successfully.set(0)
+                prom_scrape_avalanche_up.set(0)
 
         time.sleep(1)
